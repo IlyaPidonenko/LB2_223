39f668f9fcd66ee239f81608adad8079504824c4
ilya Pidonenko <ilja.pidonekom@gmail.com>
2024-01-28 16:03:20 +0100

    started with user logic, added code for user class and -service

---
+++ models/user.ts
@@ -0,0 +1,22 @@
+export class User {
+    private id: number;
+    private username: string;
+    private password: string;
+  
+    constructor(id: number, username: string, password: string) {
+      this.id = id;
+      this.username = username;
+      this.password = password;
+    }
+  
+    // get method for the usernames
+    get getUsername(): string {
+      return this.username;
+    }
+  
+    // method to check the passwords
+    public checkPassword(inputPassword: string): boolean {
+      return this.password === inputPassword;
+    }
+  }
+  
